87ae1c1401d3bf3a0e3ef3decec5666965aed925
Tejas M R <totejasmr@gmail.com>
2022-05-16 21:27:10 +0530

    Completed v1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+# Selenium Chromedriver
+
+[![PyPI version](https://badge.fury.io/py/selenium-chromedriver.svg)](https://badge.fury.io/py/selenium-chromedriver)
+
+Install Stable version Chromedriver for Selenium on Windows, MacOS, M1 MacOS and Linux
+
+## Usage
+
+```sh
+pip3 install selenium-chromedriver
+python3 -m selenium-chromedriver
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from sys import modules
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='selenium-chromedriver',  
+    version='1.0',
+    author="Tejas M R",
+    author_email="totejasmr@gmail.com",
+    description="Install Stable version Chromedriver for Selenium on Windows, MacOS, M1 MacOS and Linux",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tezz-io/selenium-chromedriver",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/tezz-io/selenium-chromedriver/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
